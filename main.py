bd8d425c1cca26271c1628195a8a29f17ac9bb2d
Isha Jain19 <isha.jain@iitgn.ac.in>
2025-08-04 12:55:12 +0530

    Add Python file for pylint testing

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 def greet(name):
-    print(f"Hello, {name}!")
+    return f"Hello, {name}"
 
 def square(n):
     return n * n
 
+def cube(n):
+    return n ** 3
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
 def main():
     name = input("Enter your name: ")
-    greet(name)
-    print("Square of 5 is:", square(5))
+    print(greet(name))
+    print("Square of 3 is:", square(3))
+    print("Cube of 3 is:", cube(3))
+    print("Factorial of 5 is:", factorial(5))
 
 if __name__ == "__main__":
     main()
